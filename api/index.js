de2b331df0da6cf2f4b786a56c8f507c45477bf9
Ayesha Shahzad <123255323+Ayshh1@users.noreply.github.com>
2024-02-07 23:43:18 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -47,6 +47,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
+app.get('*', (req, res) => {
+  res.json("hi");
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
